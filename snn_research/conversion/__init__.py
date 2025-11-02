2788d853fda0d49942511f09cee14e651c416cb4
matsushibadenki <littlebuddha@mac.com>
2025-11-02 18:08:16 +0900

    Update __init__.py

--- snn_research/conversion/__init__.py
+++ snn_research/conversion/__init__.py
@@ -1,5 +1,16 @@
-# matsushibadenki/snn4/snn_research/conversion/__init__.py
+# ファイルパス: snn_research/conversion/__init__.py
+# (更新)
 
 from .ann_to_snn_converter import AnnToSnnConverter
+# --- ▼ 追加 ▼ ---
+from .ecl_components import LearnableClippingFunction, LearnableClippingLayer
+from typing import List
+# --- ▲ 追加 ▲ ---
 
-__all__ = ["AnnToSnnConverter"]
+__all__: List[str] = [
+    "AnnToSnnConverter",
+    # --- ▼ 追加 ▼ ---
+    "LearnableClippingFunction",
+    "LearnableClippingLayer"
+    # --- ▲ 追加 ▲ ---
+]

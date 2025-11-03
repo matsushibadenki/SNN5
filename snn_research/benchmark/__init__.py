87f08f004302e6a6ad837517f65626916636230a
matsushibadenki <littlebuddha@mac.com>
2025-11-03 19:14:06 +0900

    Update __init__.py

--- snn_research/benchmark/__init__.py
+++ snn_research/benchmark/__init__.py
@@ -3,12 +3,14 @@
 # Description: ベンチマーク関連のクラスや関数を公開し、タスクレジストリを定義する。
 #
 # 修正 (v2): CIFAR10DVSTask をレジストリと __all__ に追加。
+#
+# 改善 (v3): SHDTask をレジストリと __all__ に追加。
 
 from typing import Dict, Type
 
 from .ann_baseline import ANNBaselineModel
 # --- ▼ 修正 ▼ ---
-from .tasks import SST2Task, CIFAR10Task, BenchmarkTask, MRPCTask, CIFAR10DVSTask
+from .tasks import SST2Task, CIFAR10Task, BenchmarkTask, MRPCTask, CIFAR10DVSTask, SHDTask
 # --- ▲ 修正 ▲ ---
 from .metrics import calculate_accuracy
 
@@ -18,6 +20,7 @@ TASK_REGISTRY: Dict[str, Type[BenchmarkTask]] = {
     "mrpc": MRPCTask,
     # --- ▼ 修正 ▼ ---
     "cifar10_dvs": CIFAR10DVSTask,
+    "shd": SHDTask, # SHDタスクを登録
     # --- ▲ 修正 ▲ ---
 }
 
@@ -26,6 +29,7 @@ __all__ = [
     "SST2Task", "CIFAR10Task", "MRPCTask", 
     # --- ▼ 修正 ▼ ---
     "CIFAR10DVSTask", 
+    "SHDTask", # 公開リストに追加
     # --- ▲ 修正 ▲ ---
     "calculate_accuracy", "TASK_REGISTRY", "BenchmarkTask"
 ]

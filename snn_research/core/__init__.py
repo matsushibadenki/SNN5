c7b391811ba4ec5537dacafaa973273a6824ae68
matsushibadenki <littlebuddha@mac.com>
2025-11-08 15:09:07 +0900

    Update __init__.py

--- snn_research/core/__init__.py
+++ snn_research/core/__init__.py
@@ -7,10 +7,16 @@
 # 修正 (v2): GLIFNeuron を __all__ に追加。
 #
 # 追加 (v3): SNN5改善レポートに基づき、TC_LIF, DualThresholdNeuron, ScaleAndFireNeuron を追加。
+#
+# 修正 (v_hpo_fix_importerror):
+# - snn_core.py 内で SpikingTransformer が SpikingTransformer_OldTextOnly に
+#   リネームされたため、インポート時にエイリアス(as)を使用して互換性を維持する。
 
 from .base import BaseModel
 from .sntorch_models import SpikingTransformerSnnTorch 
-from .snn_core import SNNCore, BreakthroughSNN, SpikingTransformer, SimpleSNN
+# --- ▼ 修正 (v_hpo_fix_importerror) ▼ ---
+from .snn_core import SNNCore, BreakthroughSNN, SpikingTransformer_OldTextOnly as SpikingTransformer, SimpleSNN
+# --- ▲ 修正 (v_hpo_fix_importerror) ▲ ---
 from .mamba_core import SpikingMamba
 from .trm_core import TinyRecursiveModel
 # --- ▼ 修正 ▼ ---
@@ -25,7 +31,7 @@ __all__ = [
     "BaseModel",
     "SNNCore",
     "BreakthroughSNN",
-    "SpikingTransformer",
+    "SpikingTransformer", # エイリアスにより、この名前でのエクスポートが引き続き機能する
     "SpikingMamba",
     "TinyRecursiveModel",
     "SimpleSNN",

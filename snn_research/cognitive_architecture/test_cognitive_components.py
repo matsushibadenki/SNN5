9ecc9110ff4bab314a4aada43e074c52c6ff1788
matsushibadenki <littlebuddha@mac.com>
2025-11-09 19:46:38 +0900

    Delete snn_research/cognitive_architecture/test_cognitive_components.py

--- snn_research/cognitive_architecture/test_cognitive_components.py
+++
@@ -1,8 +0,0 @@
-# ファイルパス: snn_research/cognitive_architecture/test_cognitive_components.py
-#
-# [!!DELETED!!]
-# このファイルは tests/cognitive_architecture/test_cognitive_components.py と
-# 内容が重複していました。
-# プロジェクトの一貫性を保つため、テストは `tests` ディレクトリに集約し、
-# このファイルは削除されました。
-pass
